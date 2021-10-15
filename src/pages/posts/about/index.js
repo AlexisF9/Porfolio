ced360e60f12c22d2f7f6cc972529ebc18e3d9c7
Alexis Flacher <aflacher@fr-b-tb04878.local>
2021-10-15 11:01:36 +0200

    Ajout contact + img about avec animes css

--- src/pages/posts/about/index.js
+++ src/pages/posts/about/index.js
@@ -9,10 +9,20 @@ export default function About() {
     <div className={css.about}>
       <Header title="About" subtitle="" lienAbout="/posts/about" lienContact="/posts/contact" />
 
-      <div className={css.AboutText}> 
-        <p>Actuellement en troisième année du Bachelor Développeur Web de <a href="https://www.hetic.net/" target="_blank"><span></span>HETIC</a>  à Paris, je suis en alternance dans l'agence <a href="https://www.tbwa-paris.com/en" target="_blank"><span></span>TBWA\Paris</a> en tant que développeur front-end.</p>
-        <p>Je sort d'un <a href="https://iut1.univ-grenoble-alpes.fr/iut1/mmi-formation-200348.kjsp" target="_blank"><span></span>DUT MMI</a> à Grenoble qui m'a permis d’acquérir une expertise globale sur les métiers du multimédia et de l’internet et de maîtriser des compétences techniques polyvalentes. Cette formation couvre tous les champs du digital. De la réalisation de sites internet à l’animation de communautés, de la création vidéo à la conception de contenus.</p>
-        <p>Je maîtrise différents langages informatiques tels que le HTML, CSS/SASS, JavaScript et PHP. J'ai également des connaissances dans l'outil GIT et GitHub, mais également dans la gestion de projet.</p>
+      <div className={css.AboutContent}>
+        <div className={css.content}>
+          <div>
+            <img src="../img/AlexisFlacher.jpg"/>
+            <ul>
+              <li><i class="fas fa-angle-right"></i> 20 ans</li>
+              <li><i class="fas fa-angle-right"></i> Mordu d'échecs</li>
+              <li><i class="fas fa-angle-right"></i> Grand fan de café</li>
+            </ul>
+          </div>
+          <p>Actuellement en troisième année du Bachelor Développeur Web de <a href="https://www.hetic.net/" target="_blank"><span></span>HETIC</a>  à Paris, je suis en alternance dans l'agence <a href="https://www.tbwa-paris.com/en" target="_blank"><span></span>TBWA\Paris</a> en tant que développeur front-end.</p>
+          <p>Je sort d'un <a href="https://iut1.univ-grenoble-alpes.fr/iut1/mmi-formation-200348.kjsp" target="_blank"><span></span>DUT MMI</a> à Grenoble qui m'a permis d’acquérir une expertise globale sur les métiers du multimédia et de l’internet et de maîtriser des compétences techniques polyvalentes. Cette formation couvre tous les champs du digital. De la réalisation de sites internet à l’animation de communautés, de la création vidéo à la conception de contenus.</p>
+          <p>Je maîtrise différents langages informatiques tels que le HTML, CSS/SASS, JavaScript et PHP. J'ai également des connaissances dans l'outil GIT et GitHub, mais également dans la gestion de projet.</p>
+        </div>
       </div>
       
     </div>
