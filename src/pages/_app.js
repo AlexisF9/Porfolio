eb4d653ef9e3ad73256cf9b7a7865534bd96e185
Alexis Flacher <aflacher@fr-b-tb04878.home>
2021-10-18 20:08:17 +0200

    Add favicon + lien github

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,6 +8,7 @@ function ProjectApp({ Component, pageProps }) {
             <title>Alexis Flacher</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"></link>
+            <link rel="icon" type="image/png" href="../../../img/favicon.png" />
         </Head>
         <Component {...pageProps} />
         </>
