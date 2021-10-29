d06fbd1f8f9d6b7cd7bcb6fb61b312cb0123c3f1
Alexis Flacher <aflacher@fr-b-tb04878.local>
2021-10-29 18:02:51 +0200

    Modifs style alignement text/img about

--- src/pages/posts/about/index.js
+++ src/pages/posts/about/index.js
@@ -23,16 +23,21 @@ export default function About() {
         <div className={css.content}>
           <div>
             <img alt="" src="../img/AlexisFlacher.jpg"/>
+            {/*
             <ul>
               <li><i class="fas fa-angle-right"></i> 20 ans</li>
               <li><i class="fas fa-angle-right"></i> Mordu d'échecs</li>
               <li><i class="fas fa-angle-right"></i> Grand fan de café</li>
             </ul>
+            */}
           </div>
-          <p>Actuellement en troisième année du Bachelor Développeur Web de <a href="https://www.hetic.net/"  ><span></span>HETIC</a>  à Paris, je suis en alternance dans l'agence <a href="https://www.tbwa-paris.com/en"  ><span></span>TBWA\Paris</a> en tant que développeur front-end.</p>
-          <i class="fab fa-github"></i> <a className={css.gitHub} href="https://github.com/AlexisF9"  >Mon GitHub</a>
-          <p>Je sort d'un <a href="https://iut1.univ-grenoble-alpes.fr/iut1/mmi-formation-200348.kjsp"  ><span></span>DUT MMI</a> à Grenoble qui m'a permis d’acquérir une expertise globale sur les métiers du multimédia et de l’internet et de maîtriser des compétences techniques polyvalentes. Cette formation couvre tous les champs du digital. De la réalisation de sites internet à l’animation de communautés, de la création vidéo à la conception de contenus.</p>
-          <i class="fas fa-download"></i> <a className={css.monCV} href="/AlexisFlacher CV.pdf"  >Telecharger mon CV</a>
+          <div className={css.aboutText}>
+            <p>Actuellement en troisième année du Bachelor Développeur Web de <a href="https://www.hetic.net/"  ><span></span>HETIC</a>  à Paris, je suis en alternance dans l'agence <a href="https://www.tbwa-paris.com/en"  ><span></span>TBWA\Paris</a> en tant que développeur front-end.</p>
+            <i class="fab fa-github"></i> <a className={css.gitHub} href="https://github.com/AlexisF9"  >Mon GitHub</a>
+            <p>Je sort d'un <a href="https://iut1.univ-grenoble-alpes.fr/iut1/mmi-formation-200348.kjsp"  ><span></span>DUT MMI</a> à Grenoble qui m'a permis d’acquérir une expertise globale sur les métiers du multimédia et de l’internet et de maîtriser des compétences techniques polyvalentes. Cette formation couvre tous les champs du digital. De la réalisation de sites internet à l’animation de communautés, de la création vidéo à la conception de contenus.</p>
+            <i class="fas fa-download"></i> <a className={css.monCV} href="/AlexisFlacher CV.pdf"  >Telecharger mon CV</a>
+          </div>
+          
         </div>
       </div>
       
