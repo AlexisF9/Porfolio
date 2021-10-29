263184b7723f8c31ed229521151089c5e7715f29
Alexis Flacher <aflacher@fr-b-tb04878.local>
2021-10-29 17:24:08 +0200

    Ajout meta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,7 @@ class MyDocument extends Document {
     return (
       <Html lang="fr">
         <Head>
+            <meta name="description" content="Bienvenue sur mon e-portfolio où je vous présente mes réalisations en développement et webdesign."/>
             <meta name="format-detection" content="telephone=no"/>
         </Head>
         <body>
