b98410d43f6b7f5bffbc5d3887c7f9afb12ac7c7
Alexis Flacher <aflacher@fr-b-tb04878.local>
2021-12-10 18:05:11 +0100

    meta google

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,7 @@ function ProjectApp({ Component, pageProps }) {
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"></link>
             <link rel="icon" type="image/png" href="../../../img/favicon.png" />
+            <meta name="google-site-verification" content="LQbWiek7FeGID3McWK0S2aTRawSMYcpkkUPGWAz3lXE" />
         </Head>
         <Component {...pageProps} />
         </>
