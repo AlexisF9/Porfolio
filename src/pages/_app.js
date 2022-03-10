71c0ccbc356906f4aa83010e3a820aa2f3379e81
Alexis Flacher <alexis.flacher38@gmail.com>
2022-03-10 12:19:01 +0100

    add meta

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,10 +7,13 @@ function ProjectApp({ Component, pageProps }) {
         <>
         <Head>
             <title>Alexis Flacher</title>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta name="description" content="Je suis développeur front-end et webdesigner et bienvenue sur mon portfolio !" />
+            <meta name="robots" content="index, follow"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta charset="UTF-8"/>
+            <meta name="google-site-verification" content="LQbWiek7FeGID3McWK0S2aTRawSMYcpkkUPGWAz3lXE" />
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"></link>
             <link rel="icon" type="image/png" href="../../../img/favicon.png" />
-            <meta name="google-site-verification" content="LQbWiek7FeGID3McWK0S2aTRawSMYcpkkUPGWAz3lXE" />
         </Head>
         <Component {...pageProps} />
         <Footer/>
