92eaf479cbbf618a3e63007ae1c67fcfaafc01fd
AlexisF9 <alexis.flacher@tbwa-paris.com>
2022-05-11 11:30:24 +0200

    add img mobile modal + modif width homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,11 @@ export default function Home({ realisations }) {
   const image2 = createRef();
   const image3 = createRef();
   const listImg = createRef();
+
   const imageModal = createRef();
+  const imageModal2 = createRef();
+  const imageModal3 = createRef();
+
   const title = createRef();
   const text = createRef();
   const modal = createRef();
@@ -47,60 +51,69 @@ export default function Home({ realisations }) {
         lienAbout="/about"
       />
       <div className={css.content}>
-        <div>
-          <h2>Mes réalisations :</h2>
+        <div className={css.projects}>
+          <div>
+            <h2>Mes réalisations :</h2>
 
-          {realisations.map((rea, index) => {
-            return (
-              <div className={css.listeLien} key={index}>
-                <Link href={`/`}>
-                  <a
-                    className={css.lienRea}
-                    onMouseEnter={() => {
-                      image.current.src = rea.img;
-                      image2.current.src = rea.img2;
-                      image3.current.src = rea.img3;
-                      listImg.current.classList.add(css.active);
+            {realisations.map((rea, index) => {
+              return (
+                <div className={css.listeLien} key={index}>
+                  <Link href={`/`}>
+                    <a
+                      className={css.lienRea}
+                      onMouseEnter={() => {
+                        image.current.src = rea.img;
+                        image2.current.src = rea.img2;
+                        image3.current.src = rea.img3;
+                        listImg.current.classList.add(css.active);
 
-                      imageModal.current.src = rea.img;
-                      text.current.innerHTML = rea.text;
-                      title.current.innerHTML = rea.titre;
-                    }}
-                    onClick={() => {
-                      modal.current.classList.add(css.open);
-                      overlay.current.classList.add(css.overlayOpen);
-                    }}
-                  >
-                    <i class="fas fa-angle-right"></i> {rea.titre}
-                  </a>
-                </Link>
-                {rea.url_repo != null && (
-                  <Link href={rea.url_repo}>
-                    <a className={css.lienRepo}>
-                      <i class="fas fa-link"></i>
+                        imageModal.current.src = rea.img;
+                        imageModal2.current.src = rea.img2;
+                        imageModal3.current.src = rea.img3;
+
+                        text.current.innerHTML = rea.text;
+                        title.current.innerHTML = rea.titre;
+                      }}
+                      onClick={() => {
+                        modal.current.classList.add(css.open);
+                        overlay.current.classList.add(css.overlayOpen);
+                      }}
+                    >
+                      <i class="fas fa-angle-right"></i> {rea.titre}
                     </a>
                   </Link>
-                )}
-                <ul>
-                  {rea.tags.map((tag, index) => {
-                    return <li key={index}>{tag}</li>;
-                  })}
-                </ul>
-              </div>
-            );
-          })}
-        </div>
-        <div
-          className={css.listImages}
-          ref={listImg}
-          onClick={() => {
-            modal.current.classList.add(css.open);
-            overlay.current.classList.add(css.overlayOpen);
-          }}
-        >
-          {image2 != null && <img alt="" className={css.image2} ref={image2} />}
-          <img alt="" className={css.image} ref={image} />
-          {image3 != null && <img alt="" className={css.image3} ref={image3} />}
+                  {rea.url_repo != null && (
+                    <Link href={rea.url_repo}>
+                      <a className={css.lienRepo}>
+                        <i class="fas fa-link"></i>
+                      </a>
+                    </Link>
+                  )}
+                  <ul>
+                    {rea.tags.map((tag, index) => {
+                      return <li key={index}>{tag}</li>;
+                    })}
+                  </ul>
+                </div>
+              );
+            })}
+          </div>
+          <div
+            className={css.listImages}
+            ref={listImg}
+            onClick={() => {
+              modal.current.classList.add(css.open);
+              overlay.current.classList.add(css.overlayOpen);
+            }}
+          >
+            {image2 != null && (
+              <img alt="" className={css.image2} ref={image2} />
+            )}
+            <img alt="" className={css.image} ref={image} />
+            {image3 != null && (
+              <img alt="" className={css.image3} ref={image3} />
+            )}
+          </div>
         </div>
       </div>
 
@@ -127,6 +140,8 @@ export default function Home({ realisations }) {
           <h3 ref={title}></h3>
           <p ref={text}></p>
           <img alt="" className={css.imageModal} ref={imageModal} />
+          <img alt="" className={css.imageModal2} ref={imageModal2} />
+          <img alt="" className={css.imageModal3} ref={imageModal3} />
         </div>
       </div>
     </div>
